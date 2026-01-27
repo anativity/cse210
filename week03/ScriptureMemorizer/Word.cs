b3d6a78e69057573a92915a1ae2fc16e5159130e
anativity <foxzep@gmail.com>
2026-01-27 18:29:41 -0500

    week 3 after team meeting

---
+++ week03/ScriptureMemorizer/Word.cs
@@ -0,0 +1,29 @@
+using System;
+
+public class Word
+{
+    public Word(string text)
+    {
+        
+    }
+
+    public void Hide()
+    {
+        
+    }
+
+    public void Show()
+    {
+        
+    }
+
+    public bool IsHidden()
+    {
+        
+    }
+
+    public string GetDisplayText()
+    {
+        
+    }
+}
